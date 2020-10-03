71a7e3b033af8bd7b5dc112a156e3617d7ceba79
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-02 23:49:09 -0500

    fix: package.json fix; added packages and starter scripts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Barebones React</h1>
-      <div>This is it...</div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Barebones React</h1>
+            <div>This is it...</div>
+        </div>
+    );
 };
 
 export default App;
